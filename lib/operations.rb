bdf17c5170e4004bea6c30e27ab397a2ed646ac4
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-03 19:00:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,13 @@
 def unsafe?(speed)
-
+  return true if speed>60 || if speed <40 &&
+  return false if 40<speed<60
 end
 
 
 
 def not_safe?(speed)
+  return true if speed > 60 || if speed <40
+  && return false if 40>speed<60
 	
 end
 	
